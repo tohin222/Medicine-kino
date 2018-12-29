8a202286a08758f5e49fc587a4547396dc175068
tohin222 <vampirehaunted24@gmail.com>
2018-12-29 21:47:00 +0600

    just admin panel add

---
+++ resources/views/backend/partials/js.blade.php
@@ -0,0 +1,11 @@
+<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+<script src="{{asset('backend/assets/js/main.js')}}"></script>
+<!-- Gmaps -->
+<!--<script src="https://maps.googleapis.com/maps/api/js?v=3&sensor=false"></script>-->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2jlT6C_to6X1mMvR9yRWeRvpIgTXgddM"></script>
+
+<script src="{{asset('backend/assets/js/lib/gmap/gmaps.js')}}"></script>
+<script src="{{asset('backend/assets/js/init/gmap-init.js')}}"></script>
