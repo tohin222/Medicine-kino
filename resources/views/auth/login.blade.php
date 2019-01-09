4cde21f4dae2a77727a3256a528367012064538e
tohin222 <vampirehaunted24@gmail.com>
2019-01-09 22:31:20 +0600

    working login and registration panel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,81 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!-- Mirrored from spruko.com/demo/kharna/html/Kharna_Admin-light/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 31 Dec 2018 06:39:54 GMT -->
+<head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Kharna-Admin Dashboard</title>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+		@include('backend.partials.css')
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+	</head>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+	<body class="bg-primary">
+		<div id="app">
+			<section class="section section-2">
+                <div class="container">
+					<div class="row">
+						<div class="single-page single-pageimage construction-bg cover-image " data-image-src="{{asset('backend/assets/img/news/img14.jpg')}}">
+							<div class="row">
+								<div class="col-lg-6">
+									<div class="wrapper wrapper2">
+										<form id="login" class="card-body" tabindex="500" method="POST" action="{{ route('login') }}">
+											@csrf
+											<h3>Login</h3>
+											<div class="mail">
+												<input  id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+												 @if ($errors->has('email'))
+												     <span class="invalid-feedback" role="alert">
+												         <strong>{{ $errors->first('email') }}</strong>
+												     </span>
+												 @endif
+												<label>Email</label>
+											</div>
+											<div class="passwd">
+												<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+												@if ($errors->has('password'))
+														<span class="invalid-feedback" role="alert">
+																<strong>{{ $errors->first('password') }}</strong>
+														</span>
+												@endif
+												<label>Password</label>
+											</div>
+											<div class="submit">
+												{{-- <a  class="btn btn-primary btn-block">Login</a> --}}
+												<button type="submit" class="btn btn-primary">
+														{{ __('Login') }}
+												</button>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+											</div>
+											<p class="mb-2">	<a  href="{{ route('password.request') }}">
+														{{ __('Forgot Your Password?') }}
+												</a></p>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+											<p class="text-dark mb-0">Don't have account?<a href="{{url('/register')}}" class="text-primary ml-1">Sign UP</a></p>
+												<p class="mb-2"><a href="{{url('/')}}" >Return home</a></p>
+										</form>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+									</div>
+								</div>
+								<div class="col-lg-6">
+									<div class="log-wrapper text-center">
+										<img src="{{asset('backend/assets/img/brand/logo-white.png')}}" class="mb-2 mt-4 mt-lg-0 " alt="logo">
+										<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure</p>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+	@include('backend.partials.js')
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+	</body>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!-- Mirrored from spruko.com/demo/kharna/html/Kharna_Admin-light/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 31 Dec 2018 06:39:56 GMT -->
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,93 +1,107 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<!-- Mirrored from spruko.com/demo/kharna/html/Kharna_Admin-light/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 31 Dec 2018 06:39:54 GMT -->
+<head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Kharna-Admin Dashboard</title>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+		@include('backend.partials.css')
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+	</head>
 
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+	<body class="bg-primary">
+		<div id="app">
+			<section class="section section-2">
+                <div class="container">
+					<div class="row">
+						<div class="single-page single-pageimage construction-bg cover-image " data-image-src="{{asset('backend/assets/img/news/img14.jpg')}}">
+							<div class="row">
+								<div class="col-lg-8">
+									<div class="wrapper wrapper2">
+										<form id="login" class="card-body" tabindex="500" method="POST" action="{{ route('register') }}">
+											@csrf
+											<h3>Login</h3>
+											<div class="mail">
+												<input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+												@if ($errors->has('name'))
+														<span class="invalid-feedback" role="alert">
+																<strong>{{ $errors->first('name') }}</strong>
+														</span>
+												@endif
+												<label>Name</label>
+											</div>
+											<div class="mail">
+												<input  id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+												@if ($errors->has('email'))
+														<span class="invalid-feedback" role="alert">
+																<strong>{{ $errors->first('email') }}</strong>
+														</span>
+												@endif
+												<label>Email</label>
+											</div>
+											<div class="mail">
+												<select class="form-control{{ $errors->has('role') ? ' is-invalid' : '' }}" name="role">
+													<option value="">--Select One--</option>
+													<option value="1">Admin</option>
+													<option value="2">SalesMan</option>
+												</select>
+												@if ($errors->has('role'))
+														<span class="invalid-feedback" role="alert">
+																<strong>{{ $errors->first('role') }}</strong>
+														</span>
+												@endif
+												<label>Role</label>
+											</div>
+											<div class="passwd">
+												<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+												@if ($errors->has('password'))
+														<span class="invalid-feedback" role="alert">
+																<strong>{{ $errors->first('password') }}</strong>
+														</span>
+												@endif
+												<label>Password</label>
+											</div>
+											<div class="passwd">
+												<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+												@if ($errors->has('password'))
+														<span class="invalid-feedback" role="alert">
+																<strong>{{ $errors->first('password') }}</strong>
+														</span>
+												@endif
+												<label>Confirm Password</label>
+											</div>
+											<div class="submit">
+												{{-- <a class="btn btn-primary btn-block" href="index.html">Register</a> --}}
+												<button type="submit" class="btn btn-primary">
+														{{ __('Register') }}
+												</button>
+											</div>
+											<p class="mb-2"><a href="{{url('/')}}" >Return home</a></p>
+											<p class="text-dark mb-0">If you have already account?<a href="{{url('/login')}}" class="text-primary ml-1">Login</a></p>
+										</form>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+									</div>
+								</div>
+								<div class="col-lg-4">
+									<div class="log-wrapper text-center">
+										<img src="{{asset('backend/assets/img/brand/logo-white.png')}}" class="mb-2 mt-4 mt-lg-0 " alt="logo">
+										<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure</p>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+	@include('backend.partials.js')
 
-                            <div class="col-md-6">
-                                <select class="form-control{{ $errors->has('role') ? ' is-invalid' : '' }}" name="role">
-                                  <option value="">--Select One--</option>
-                                  <option value="1">Admin</option>
-                                  <option value="2">SalesMan</option>
-                                </select>
-                                @if ($errors->has('role'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('role') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                      
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+	</body>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!-- Mirrored from spruko.com/demo/kharna/html/Kharna_Admin-light/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 31 Dec 2018 06:39:56 GMT -->
+</html>
